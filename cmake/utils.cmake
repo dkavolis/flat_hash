33cd3b8ff7d7aaa9556c1ad495bce013b4ed00a4
D. Kavolis <12998363+dkavolis@users.noreply.github.com>
2022-05-05 16:41:27 +0300

    fix CodeCoverage.cmake fails to find gcovr from visual studio in wsl

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -15,13 +15,14 @@ function(enable_coverage NAME_ TARGET)
   list(GET version 0 gcc_major)
   get_filename_component(PATH ${CMAKE_CXX_COMPILER} DIRECTORY)
   find_program(GCOV NAMES gcov-${gcc_major} gcov HINTS ${path} PATHS ENV GCOV)
-  find_program(GCOVR NAMES gcovr)
+  find_program(GCOVR NAMES gcovr PATHS ~/.local/bin /usr/bin ${CMAKE_SOURCE_DIR}/scripts/test ENV GCOVR)
   message(STATUS "GCOVR = ${GCOVR}")
 
   if(GCOV)
     message(STATUS "Code coverage enabled")
 
     include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CodeCoverage.cmake)
+    set(GCOVR_PATH ${GCOVR} CACHE "FILEPATH" "Path to gcovr" FORCE)
     append_coverage_compiler_flags_to_target(${TARGET})
     # no-exceptions to get rid of exception handling branches
     target_compile_options(
